6629943dfc9b05c48df01445159bf35e7e245a79
Jannik Hiller <jannik.hiller@live.de>
2020-12-14 15:39:22 +0100

    bla

--- zettel04/Launcher.java
+++ zettel04/Launcher.java
@@ -5,10 +5,10 @@
 public class Launcher {
     public static void main(String[] args) {
         Product[] testProducts = { new Product("Apfel",      "111111001111", "111111111111"),
-                                   new Product("Banane",     "000000000000", "111111111111"), 
-                                   new Product("Erdbeere",   "000011100000", "000110000000"), 
-                                   new Product("Blumenkohl", "000001111100", "111100000011"), 
-                                   new Product("Feldsalat",  "100011001111", "111100001111"), 
+                                   new Product("Banane",     "000000000000", "111111111111"),
+                                   new Product("Erdbeere",   "000011100000", "000110000000"),
+                                   new Product("Blumenkohl", "000001111100", "111100000011"),
+                                   new Product("Feldsalat",  "100011001111", "111100001111"),
                                    new Product("Zwiebel",    "111110011111", "001111100000") };
 
         System.out.println(new SeasonalCalendar(testProducts).stringify());
